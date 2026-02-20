4ff7246a5286a55604db8af70f7accb5017016cf
Adan Martinez <adanarturomartinez@gmail.com>
2026-02-19 23:22:22 -0600

    Updating Files and adding automation scripts

--- Automation_OpenWebPageCommands.py
+++ Automation_OpenWebPageCommands.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get("https://www.google.com")
-
+driver.get("https://www.amazon.com.mx")
+driver.title
+print(driver.title)
+driver.maximize_window()
 # El código se detendrá aquí y el navegador se quedará abierto
 input("Presiona Enter en la terminal para cerrar el navegador...")
 
